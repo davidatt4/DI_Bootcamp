64da332b16ca1aa3a1a1f133caf470d6850649c1
redoignon <150930791+redoignon@users.noreply.github.com>
2023-12-07 09:43:33 +0200

    Add files via upload

---
+++ Week 2 DI/day_1/daily_challenge_w2_d1.py
@@ -0,0 +1,30 @@
+class Farm:
+    def __init__(self,farm_name):
+        self.farm_name = farm_name
+        self.animals={}
+    def add_animal(self,animal,quantity=1):
+        if animal in self.animals:
+            self.animals[animal]+=quantity
+        else:
+            self.animals[animal]=quantity
+
+    def get_info(self):
+        farm_info=f"{self.farm_name}'s farm\n"
+        for animal,quantity in self.animals.items():
+            farm_info+=f"{animal}:{quantity}\n"
+
+            farm_info="E-I-E-I-O"
+            return farm_info
+    def get_animal_types(self):
+        return sorted(list(self.animals.keys()))
+    def get_short_info(self):
+        animal_types=self.get_animal_types()
+        animal_str=','.join(animal_types)
+        return f"{self.farm_name}'s farm has {animal_str}."
+
+macdonald = Farm("McDonald")
+macdonald.add_animal('cow',5)
+macdonald.add_animal('sheep')
+macdonald.add_animal('sheep')
+macdonald.add_animal('goat', 12)
+print(macdonald.get_short_info())
